604b40dc820ff273af55a032e5203d6807e299a5
PenyiDavid <penyaskodavid@gmail.com>
2025-02-28 11:45:02 +0100

    Orders, chocolates

--- csokiszallitas/app/Http/Controllers/OrderController.php
+++ csokiszallitas/app/Http/Controllers/OrderController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chocolate;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function index(){
+        $orders = Order::all();
+        return response()->json($orders);
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'email'=>'required|string',
+            'address'=>'required|string',
+            'chocolate_brand'=>'required|string',
+            'chocolate_name'=>'required|string',
+            'count'=>'required|numeric',
+        ]);
+        $chocolate = Chocolate::where('brand',$request->chocolate_brand)
+                                ->where('chocolate_name',$request->chocolate_name)
+                                ->first(); //query builder
+
+        $all_price = $chocolate->price * $request->count;
+        Order::create([
+            'email'=>$request->email,
+            'address'=>$request->address,
+            'chocolate_id'=>$chocolate->id,
+            'count'=>$request->count,
+            'all_price'=>$all_price,
+        ]);
+        return response()->json(['success'=>true,'message'=>'Rekord sikeresen hozzáadva!'],201,['Access-Control-Allow-Origin'=>'*'],JSON_UNESCAPED_UNICODE);
+    }
 }
---
+++ csokiszallitas/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\ChocolateController;
+use App\Http\Controllers\OrderController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/orders', [OrderController::class, 'index']);
+Route::post('/orders', [OrderController::class, 'store']);
+
+Route::get('/chocolates', [ChocolateController::class, 'index']);
+Route::post('/chocolates', [ChocolateController::class, 'store']);
